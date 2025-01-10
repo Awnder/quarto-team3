db68b4b6a63b7071a1d8be54817fc3400051e12e
cruseaea <143053181+cruseaea@users.noreply.github.com>
2025-01-10 11:32:19 -0800

    Update andrew_functions.py

--- andrew_functions.py
+++ andrew_functions.py
@@ -18,19 +18,50 @@ def draw_hollow_purple_small_square(canvas: object, startx: int, starty: int) ->
 def draw_solid_purple_large_circle(canvas: object, startx: int, starty: int) -> None:
   canvas.create_oval(startx, starty, startx+200, starty+200, fill="purple")
 
+# Cruse
+def draw_solid_green_small_circle(canvas: object, startx: int, starty: int) -> None:
+    canvas.create_oval(startx, starty, startx+50, starty+50, fill="green")
+
+def draw_solid_purple_small_circle(canvas: object, startx: int, starty: int) -> None:
+    canvas.create_oval(startx, starty, startx+50, starty+50, fill="purple")
+  
+def draw_hollow_green_large_circle(canvas: object, startx: int, starty: int) -> None:
+    canvas.create_oval(startx, starty, startx+200, starty+200, fill="green", outline="green", width=2)
+    canvas.create_oval(startx+50, starty+50, startx+150, starty+150, fill="white", outline="green", width=2)
+  
+def draw_hollow_purple_large_circle(canvas: object, startx: int, starty: int) -> None:
+    canvas.create_oval(startx, starty, startx+200, starty+200, fill="purple", outline="purple", width=2)
+    canvas.create_oval(startx+50, starty+50, startx+150, starty+150, fill="white", outline="purple", width=2)
+
+def draw_hollow_green_small_circle(canvas: object, startx: int, starty: int) -> None:
+    canvas.create_oval(startx, starty, startx+50, starty+50, fill="green", outline="green", width=2)
+    canvas.create_oval(startx+15, starty+15, startx+35, starty+35, fill="white", outline="green", width=2)
+
+def draw_hollow_purple_small_circle(canvas: object, startx: int, starty: int) -> None:
+    canvas.create_oval(startx, starty, startx+50, starty+50, fill="purple", outline="purple", width=2)
+    canvas.create_oval(startx+15, starty+15, startx+35, starty+35, fill="white", outline="purple", width=2)
+
 def test():
   root = tk.Tk()
   root.title('Shapes')
-  root.geometry('500x500')
+  root.geometry('700x700')
 
   canvas = tk.Canvas()
   canvas.pack(fill=tk.BOTH, expand=1)
   canvas.configure(bg="white")
 
   draw_solid_purple_large_square(canvas, 0, 0)
-  draw_hollow_purple_large_square(canvas, 0, 0)
-  draw_hollow_purple_small_square(canvas, 300, 300)
-  draw_solid_purple_large_circle(canvas, 2, 2)
+  draw_hollow_purple_large_square(canvas, 250, 0)
+  draw_hollow_purple_small_square(canvas, 500, 0)
+  draw_solid_purple_large_circle(canvas, 0, 250)
+
+  # Cruse
+  draw_solid_green_small_circle(canvas, 100, 500)
+  draw_solid_purple_small_circle(canvas, 100, 600)
+  draw_hollow_green_large_circle(canvas, 250, 250)
+  draw_hollow_purple_large_circle(canvas, 500, 250)
+  draw_hollow_green_small_circle(canvas, 250, 500)
+  draw_hollow_purple_small_circle(canvas, 400, 500)
 
   root.mainloop()
 
